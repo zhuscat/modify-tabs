817985e541a5563b4e8d8479d52deec9020d7743
zhuscat <zhudx6512@163.com>
2021-12-06 17:20:58 +0800

    feat: 第一个版本

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const got = require('got').default
+const fs = require('fs/promises')
+const path = require('path')
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const imageUrl = core.getInput('url')
+    core.debug(`图片地址 ${imageUrl}`)
+    const opType = core.getInput('op_type')
+    core.debug(`操作类型 ${opType}`)
+    const body = await got.get(imageUrl).buffer()
+    await fs.writeFile(path.join(__dirname, `./src/static/tab/cts_tab.png`), body)
+    core.debug(`写入文件`)
+    const pagesFile = await fs.readFile(path.join(__dirname, './src/pages.json'))
+    const pages = JSON.parse(pagesFile)
+    if (opType === 'WRITE') {
+      const idx = pages.tabBar.list.findIndex(item => item.pagePath === 'pages/cts/index')
+      if (idx > 0) {
+        // 修改已经存在的
+        pages.tabBar.list[idx].text = 'CTS'
+        pages.tabBar.list[idx].iconPath = 'static/tab/cts_tab.png'
+        pages.tabBar.list[idx].selectedIconPath = 'static/tab/cts_tab.png'
+      } else {
+        const len = pages.tabBar.list.length
+        const insertIdx = Math.floor(len / 2)
+        core.debug(`插入到第 ${insertIdx + 1} 个 tab`)
+        pages.tabBar.list.splice(insertIdx, 0, {
+          pagePath: 'pages/cts/index',
+          text: 'CTS',
+          iconPath: 'static/tab/cts_tab.png',
+          selectedIconPath: 'static/tab/cts_tab.png'
+        })
+      }
+    } else {
+      pages.tabBar.list = pages.tabBar.list.filter(item => item.pagePath !== 'pages/cts/index')
+      core.debug(`移除 tab 成功`)
+    }
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    fs.writeFile(path.join(__dirname, `./src/pages.json`), JSON.stringify(pages, null, 2))
+    core.debug(`更新 tab 成功`)
+    core.setOutput()
   } catch (error) {
     core.setFailed(error.message);
   }
--- index.test.js
+++ index.test.js
@@ -1,24 +1,24 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+// const process = require('process');
+// const cp = require('child_process');
+// const path = require('path');
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
+// test('throws invalid number', async () => {
+//   await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
+// });
 
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
+// test('wait 500 ms', async () => {
+//   const start = new Date();
+//   await wait(500);
+//   const end = new Date();
+//   var delta = Math.abs(end - start);
+//   expect(delta).toBeGreaterThanOrEqual(500);
+// });
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
-  const ip = path.join(__dirname, 'index.js');
-  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
-  console.log(result);
+  // process.env['INPUT_URL'] = "https://cdn.qiandaoapp.com/admins/17b9d03abdacc7435814427db7f5ec6e.png";
+  // process.env['INPUT_OP_TYPE'] = 'WRITE'
+  // const ip = path.join(__dirname, 'index.js');
+  // const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+  // console.log(result);
 })
