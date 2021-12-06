3a2d7e8796750f26ff468a78f499e603d7d2d877
zhuscat <zhudx6512@163.com>
2021-12-06 21:40:38 +0800

    feat: gen dist

--- dist/index.js
+++ dist/index.js
@@ -9487,39 +9487,44 @@ const path = __nccwpck_require__(5622)
 async function run() {
   try {
     const imageUrl = core.getInput('url')
-    core.debug(`当前目录 ${__dirname}`)
     core.debug(`图片地址 ${imageUrl}`)
     const opType = core.getInput('op_type')
     core.debug(`操作类型 ${opType}`)
+    const src = core.getInput('src')
+    const dist = core.getInput('dist')
+    const pagePath = core.getInput('page_path')
     const body = await got.get(imageUrl).buffer()
-    await fs.writeFile(path.resolve(`./src/static/tab/cts_tab.png`), body)
+    await fs.writeFile(path.resolve(dist), body)
     core.debug(`写入文件`)
-    const pagesFile = await fs.readFile(path.resolve('./src/pages.json'))
+    const pagesFile = await fs.readFile(path.resolve(src, 'pages.json'))
     const pages = JSON.parse(pagesFile)
+    const tabName = core.getInput('tab_name')
+    const iconPath = path.relative(core.getInput('src'), dist)
     if (opType === 'WRITE') {
-      const idx = pages.tabBar.list.findIndex(item => item.pagePath === 'pages/cts/index')
+      const idx = pages.tabBar.list.findIndex(item => item.pagePath === pagePath)
       if (idx > 0) {
         // 修改已经存在的
-        pages.tabBar.list[idx].text = 'CTS'
-        pages.tabBar.list[idx].iconPath = 'static/tab/cts_tab.png'
-        pages.tabBar.list[idx].selectedIconPath = 'static/tab/cts_tab.png'
+        pages.tabBar.list[idx].text = tabName
+        pages.tabBar.list[idx].text = tabName
+        pages.tabBar.list[idx].iconPath = iconPath
+        pages.tabBar.list[idx].selectedIconPath = iconPath
       } else {
         const len = pages.tabBar.list.length
         const insertIdx = Math.floor(len / 2)
         core.debug(`插入到第 ${insertIdx + 1} 个 tab`)
         pages.tabBar.list.splice(insertIdx, 0, {
-          pagePath: 'pages/cts/index',
-          text: 'CTS',
-          iconPath: 'static/tab/cts_tab.png',
-          selectedIconPath: 'static/tab/cts_tab.png'
+          pagePath: pagePath,
+          text: tabName,
+          iconPath,
+          selectedIconPath: iconPath
         })
       }
     } else {
-      pages.tabBar.list = pages.tabBar.list.filter(item => item.pagePath !== 'pages/cts/index')
+      pages.tabBar.list = pages.tabBar.list.filter(item => item.pagePath !== pagePath)
       core.debug(`移除 tab 成功`)
     }
 
-    fs.writeFile(path.resolve(`./src/pages.json`), JSON.stringify(pages, null, 2))
+    fs.writeFile(path.resolve(src, 'pages.json'), JSON.stringify(pages, null, 2))
     core.debug(`更新 tab 成功`)
     core.setOutput()
   } catch (error) {
